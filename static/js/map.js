e33947eb48d3c10e225eb92f79404424d9b693a3
jonatas1n <jonatasgeek@gmail.com>
2024-11-30 11:27:44 -0300

    update

--- static/js/map.js
+++ static/js/map.js
@@ -1,8 +1,22 @@
 const map = L.map('map').setView([20, 0], 2);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+const imageBounds = [
+  [25, -120],
+  [40, 30],
+];
+
+const imageUrl = "/static/img/yellow.svg";
+
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+window.onresize = () => {
+  map.invalidateSize();
+};
 
 fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
   .then(response => response.json())
